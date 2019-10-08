6843b29b5147bc740a85018bfa0c102f2fb213e3
zhaoziyun <zhaoziyun890217@163.com>
2019-10-08 22:16:28 +0800

    干掉大写的字母

--- index_files/index_js.js
+++ index_files/index_js.js
@@ -11,7 +11,7 @@ function createCode() {
     var codeChars = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'); //所有候选组成验证码的字符，当然也可以用中文的
     for (var i = 0; i < codeLength; i++) {
-        var charNum = Math.floor(Math.random() * 52);
+        var charNum = Math.floor(Math.random() * 10);
         code += codeChars[charNum];
     }
     if (checkCode) {
