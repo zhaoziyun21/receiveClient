537422524a1e9fe9cbe27284fb8f32010f20d8ca
zhaoziyun <zhaoziyun890217@163.com>
2019-10-08 22:02:26 +0800

    干掉大写的字母

--- index_files/index_js.js
+++ index_files/index_js.js
@@ -5,9 +5,11 @@ function createCode() {
     code = "";
     var codeLength = 4; //验证码的长度
     var checkCode = document.getElementById("checkCode");
+    // var codeChars = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    // 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    // 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'); //所有候选组成验证码的字符，当然也可以用中文的
     var codeChars = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'); //所有候选组成验证码的字符，当然也可以用中文的
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'); //所有候选组成验证码的字符，当然也可以用中文的
     for (var i = 0; i < codeLength; i++) {
         var charNum = Math.floor(Math.random() * 52);
         code += codeChars[charNum];
